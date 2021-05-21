99264d1f0c82223e962a7bfcde85e82bce97cc8c
Vincent Långström <vincent.langstrom@gmail.com>
2021-05-21 11:41:54 +0300

    change shebang again

--- app/oracle/eeManager.py
+++ app/oracle/eeManager.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/home/pi/IoT-Microservice/venv/bin/python3
 import json
 import base64
 import time
--- app/oracle/generate_pems.py
+++ app/oracle/generate_pems.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/home/pi/IoT-Microservice/venv/bin/python3
 from eeManager import write_keys_to_file, generate_private_public_keys
 import os
 
