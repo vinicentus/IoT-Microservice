e4ad8b4260f3183ea2185010705ee5ac2e9f3586
Vincent Långström <vincent.langstrom@gmail.com>
2021-05-26 15:34:04 +0300

    add cleaning that doesn't stop emasurement

---
+++ app/oracle/sps30_sensor_clean_oneshot.py
@@ -0,0 +1,18 @@
+#!/home/pi/IoT-Microservice/venv/bin/python3
+# CHANGE PYTHON PATH TO MATCH YOUR LOCAL INSTALLATION
+import time
+from sps30_driver import sps30
+
+device_port = "/dev/ttyUSB0"
+
+
+try:
+    sensor_sps30 = sps30.Sps30(device_port)
+    # EXECUTES FAN CLEANING COMMAND
+    sfc = sensor_sps30.start_fan_cleaning()
+    time.sleep(1)
+    sensor_sps30.close_port()
+
+except TypeError as e:
+    print(e)
+    exit()
