10516bbd6d9f79603505384429d784fb4e914323
Vincent Långström <vincent.langstrom@gmail.com>
2021-08-23 10:40:47 +0300

    add todo

--- app/oracle/main.py
+++ app/oracle/main.py
@@ -13,6 +13,7 @@ def perform_task(task, params):
     try:
         # Check for invalid data
         # In the future we might want to start rate limiting if we get many tasks with invalid parameters.
+        # TODO: we should probably retry some tasks
         data = sm.execute(**params)
         print(type(data))
         return data
