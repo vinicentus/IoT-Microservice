e2daf37fda1b0cf2f4e14787d0537932050fcde3
Vincent Långström <vincent.langstrom@gmail.com>
2021-08-24 10:48:12 +0300

    autoformat

--- app/launcher.py
+++ app/launcher.py
@@ -4,6 +4,7 @@
 # In[1]:
 
 
+from oracle import main as oracle_software
 import subprocess
 import sys
 import time
@@ -61,13 +62,9 @@ web3 = blockchain_utils.connect(settings)
 # In[ ]:
 
 
-
 # ### GENERATE A FRESH WHISPER ID
 
 # In[ ]:
-
-
-
 
 
 # ### CREATE DEVICE OUTLINE
@@ -83,7 +80,8 @@ oracle = device_utils.create(device_info)
 # In[ ]:
 
 
-oracle_manager = blockchain_utils.contract(latest['oraclemanager'], web3, settings)
+oracle_manager = blockchain_utils.contract(
+    latest['oraclemanager'], web3, settings)
 
 
 # In[ ]:
@@ -113,7 +111,7 @@ temp_contract = blockchain_utils.contract({
 
 if temp_contract.address != '0x0000000000000000000000000000000000000000':
     oracle.set_contract(temp_contract)
-    
+
 else:
     print('THE ORACLE IS NOT REGISTERED, ABORTING..')
     sys.exit(0)
@@ -133,7 +131,7 @@ raw = oracle.read('fetch_backlog')
 backlog = utils.filter_backlog(raw)
 
 
-# ### GLOBAL ACTIVE STATUS 
+# ### GLOBAL ACTIVE STATUS
 
 # In[ ]:
 
@@ -169,13 +167,13 @@ discovery_config = utils.decode(encoded)
 
 
 def update_details():
-    
+
     # FETCH GLOBAL VARS
     global active
     global discoverable
     global discovery_config
     global backlog
-    
+
     # EXTRACT RELEVANT VALUES
     latest_active = oracle.read('active')
     latest_discoverable = oracle.read('discoverable')
@@ -183,31 +181,31 @@ def update_details():
 
     # IF ACTIVE STATUS HAS CHANGED
     if (latest_active != active):
-        
+
         # UPDATE ACTIVE STATUS
         active = latest_active
-        
+
         # SEND MSG
         print('ACTIVE STATUS CHANGED TO:', latest_active)
-        
+
     # IF DISCOVERABLE STATUS HAS CHANGED
     if (latest_discoverable != discoverable):
-        
+
         # UPDATE ACTIVE STATUS
         discoverable = latest_discoverable
-        
+
         # SEND MSG
         print('DISCOVERABLE STATUS CHANGED TO:', latest_discoverable)
-        
+
     # IF DISCOVERABLE STATUS HAS CHANGED
     if (latest_config != discovery_config):
-        
+
         # UPDATE ACTIVE STATUS
         discovery_config = latest_config
-        
+
         # SEND MSG
         print('DISCOVERY CONFIG CHANGED')
-        
+
     # UPDATE BACKLOG
     raw_backlog = oracle.read('fetch_backlog')
     backlog = utils.filter_backlog(raw_backlog)
@@ -219,7 +217,7 @@ def update_details():
 
 
 def update_middleware():
-    
+
     # PRINT REACTION
     print('\nMIDDLEWARE UPDATE TRIGGERED')
 
@@ -236,20 +234,20 @@ def update_middleware():
 
 
 def perform_task(task, func):
-    
+
     # SHOW MSG
     print('STARTING TASK:', task)
-    
+
     # SERIALIZE THE TASK CONTRACT
     task_contract = blockchain_utils.contract({
         'address': task,
         'abi': latest['task']['abi']
     }, web3, settings)
-    
+
     # FETCH & DECODE TASK PARAMS
     params = task_contract.read('params')
     decoded = utils.decode(params)
-    
+
     # PERFORM ORACLE TASK
     result = func(task, decoded)
 
@@ -269,42 +267,43 @@ def perform_task(task, func):
 
 
 def process_message(event):
-    
+
     # SERIALIZE EVENT PARAMS
     author = web3.toHex(event['sig'])
     payload = web3.toText(event['payload'])
 
     # DECODE THE PAYLOAD
     data = utils.decode(payload)
-    
+
     # REQUIRED KEYS FOR VALID MESSAGE
     required = ['type', 'discovery']
 
     # DECODED KEYS
     keys = list(data.keys())
-    
+
     # THE REQUEST KEYWORD FOR THE PAYLOAD TYPE
     keyword = 'request'
-    
+
     # IF THE KEYSETS MATCH & THE TYPE IS A REQUEST
     if (required == keys and data['type'] == keyword):
-        
+
         # CHECK MATCHES IN DISCOVERY PARAMS
-        discovery_result = utils.compare_discovery(data['discovery'], discovery_config)
-        
+        discovery_result = utils.compare_discovery(
+            data['discovery'], discovery_config)
+
         # IF EVERYTHING MATCHED
         if (discovery_result.count(False) == 0):
-            
+
             # SHOW MSG
             print('DISCOVERY REQUEST DETECTED')
-            
+
             # ENCODE A JSON RESPONSE
             response = utils.encode({
                 'type': 'response',
                 'source': payload,
                 'oracle': oracle.hash
             })
-            
+
             # SLEEP FOR 2 SECONDS
             time.sleep(2)
 
@@ -338,7 +337,6 @@ modification_event = oracle.event('modification')
 # In[ ]:
 
 
-
 # ### MIDDLEWARE EVENT LOOP
 
 # In[6]:
@@ -348,7 +346,6 @@ print('AWAITING EVENTS...\n')
 
 # DYNAMICALLY IMPORT THE ORACLE SOFTWARE
 #oracle_software = import_file('oracle/main.py')
-from oracle import main as oracle_software
 func = oracle_software.perform_task
 
 while(True):
@@ -369,10 +366,4 @@ while(True):
             perform_task(task, func)
 
 
-
-
 # In[ ]:
-
-
-
-
