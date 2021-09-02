55343d00d9a2d150d26abddfb0397bdf8574c08d
Vincent Långström <vincent.langstrom@gmail.com>
2021-09-02 09:28:50 +0300

    import generated contract init tool

---
+++ app/init.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import nbimporter
+
+
+# In[2]:
+
+
+import utils as utils
+import blockchain as blockchain
+
+
+# ### LOAD RESOURCES
+
+# In[3]:
+
+
+settings = utils.load_yaml('resources/settings.yaml')
+
+
+# In[4]:
+
+
+latest = utils.load_json('resources/latest.json')
+
+
+# ### CONNECT TO ETHEREUM GATEWAY
+
+# In[5]:
+
+
+web3 = blockchain.connect(settings)
+
+
+# ### SERIALIZE MANAGER CONTRACTS
+
+# In[6]:
+
+
+user_manager = blockchain.contract(latest['usermanager'], web3, settings)
+
+
+# In[7]:
+
+
+oracle_manager = blockchain.contract(latest['oraclemanager'], web3, settings)
+
+
+# In[8]:
+
+
+task_manager = blockchain.contract(latest['taskmanager'], web3, settings)
+
+
+# In[9]:
+
+
+token_manager = blockchain.contract(latest['tokenmanager'], web3, settings)
+
+
+# ### INIT TOKEN MANAGER
+
+# In[10]:
+
+
+token_symbol = 'ArcaCoin'
+token_price = 5000
+token_capacity = 10000
+
+
+# In[11]:
+
+
+token_manager.write({
+    'func': 'init',
+    'params': [
+        token_symbol,
+        token_price,
+        token_capacity,
+        task_manager.address
+    ]
+})
+
+
+# ### INIT TASK MANAGER
+
+# In[12]:
+
+
+task_token_fee = 2
+
+
+# In[13]:
+
+
+task_manager.write({
+    'func': 'init',
+    'params': [
+        task_token_fee,
+        user_manager.address,
+        oracle_manager.address,
+        token_manager.address
+    ]
+})
+
+
+# ### INIT USER MANAGER
+
+# In[14]:
+
+
+user_manager.write({
+    'func': 'init',
+    'params': [task_manager.address]
+})
+
+
+# ### INIT ORACLE MANAGER
+
+# In[15]:
+
+
+oracle_manager.write({
+    'func': 'init',
+    'params': [
+        user_manager.address,
+        task_manager.address
+    ]
+})
+
+
+# In[ ]:
+
+
+
+
