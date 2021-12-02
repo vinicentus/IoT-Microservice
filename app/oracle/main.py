358a4e5eb37051f2779c50cba61dd131375b7c66
Vincent Långström <vincent.langstrom@gmail.com>
2021-12-02 15:18:58 +0200

    choose between storj and old task handler

--- app/oracle/main.py
+++ app/oracle/main.py
@@ -8,15 +8,27 @@ from . import storj_test as storj_manager
 # In[1]:
 
 
-def perform_task(task, params):
+def perform_task(task: str, params: dict):
     print('\tORACLE TRIGGERED')
     print(params)
     try:
         # Check for invalid data
         # In the future we might want to start rate limiting if we get many tasks with invalid parameters.
         # TODO: we should probably retry some tasks
-        # data = sm.execute(**params)
-        data = storj_manager.execute_storj() # Use experimental storj service manager TODO: add params
+
+        # Parse data to select handler
+        if (params['task_return_type'] == 'storj'):
+            print('Task type is STORJ!')
+
+            # TODO: add params
+            # Use experimental storj service manager
+            data = storj_manager.execute_storj()
+        else:
+            # Remove new task_return_type value because the old execute function can't handle it
+            params.pop('task_return_type')
+
+            # Use original ethereum-based upload method
+            data = sm.execute(**params)
         return data
     except TypeError as error:
         # TODO: format this as base64 and possibly encrypt it as well!
